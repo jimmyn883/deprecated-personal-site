2596907493388e4a3837a8487f8d027e7b770d6d
jimmyn883 <55668719+jimmyn883@users.noreply.github.com>
2025-05-19 00:04:27 -0600

    BugFix: Allow clicking on portfolio links

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,40 +1,43 @@
-import React, { Component } from 'react';
+export default function Portfolio({ data }) {
+    if (!data) return null;
 
-export default function Portfolio({data}) {
-  if(data){
-    var projects = data.projects.map(function(projects){
-      var projectImage = 'images/portfolio/'+projects.image;
-      return <div key={projects.title} className="columns portfolio-item">
-          <div className="item-wrap">
-          <a href={projects.url} title={projects.title}>
-              <img alt={projects.title} src={projectImage} />
-              <div className="overlay">
-                <div className="portfolio-item-meta">
-                <h5>{projects.title}</h5>
-                    <p>{projects.category}</p>
+    const projects = data.projects.map((project) => {
+        const projectImage = 'images/portfolio/' + project.image;
+        return (
+            <div key={project.title} className="columns portfolio-item">
+                <div className="item-wrap">
+                    <img alt={project.title} src={projectImage} />
+                    <div className="overlay">
+                        <div className="portfolio-item-meta">
+                            <h5>{project.title}</h5>
+                            <p>{project.category}</p>
+                        </div>
+                        <div className="link-icon">
+                            <a
+                                href={project.url}
+                                title={project.title}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <i className="fa fa-link" />
+                            </a>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            <div className="link-icon"><i className="fa fa-link"></i></div>
-          </a>
-        </div>
-      </div>
-    })
-  }
+            </div>
+        );
+    });
 
   return (
-    <section id="portfolio">
-
-    <div className="row">
-
-        <div className="twelve columns collapsed">
-
-          <h1>Check Out Some of My Works.</h1>
-
-          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-              {projects}
+      <section id="portfolio">
+          <div className="row">
+              <div className="twelve columns collapsed">
+                  <h1>Check Out Some of My Works.</h1>
+                  <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+                      {projects}
+                  </div>
+              </div>
           </div>
-        </div>
-    </div>
-  </section>
+      </section>
   );
 }
